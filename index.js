7d856bb55d27ffa559ba5f918d3d430eeac86126
Badsha-Nishan <badshanisan14@gmail.com>
2026-06-25 22:45:57 +0600

    initialized with createUsers api

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("lifedrop");
+    const usersCollection = database.collection("users");
+
+    // Create Users Data
+    app.post("/api/users", async (req, res) => {
+      try {
+        const usersData = req.body;
+
+        console.log("Received:", usersData);
+
+        const result = await usersCollection.insertOne(usersData);
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({
+          success: false,
+          message: error.message,
+        });
+      }
+    });
+
+    // Logged User Email
+    app.get("/api/users/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const result = await usersCollection.findOne({
+        email,
+      });
+
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
