2233b9951d953410b162c6d0fd561809d44aa919
Badsha-Nishan <badshanisan14@gmail.com>
2026-06-27 16:25:51 +0600

    updated

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("lifedrop");
     const usersCollection = database.collection("users");
